5aa53974f15364f90abe217fee026e321842bac8
Reto Stauffer <reto.stauffer@uibk.ac.at>
2017-12-19 14:30:52 +0100

    update

--- migrate.py
+++ migrate.py
@@ -7,13 +7,13 @@
 # -------------------------------------------------------------------
 # - EDITORIAL:   2014-09-14, RS: Created file on thinkreto.
 # -------------------------------------------------------------------
-# - L@ST MODIFIED: 2015-08-04 09:33 on prognose2.met.fu-berlin.de
+# - L@ST MODIFIED: 2017-12-19 13:03 on thinkreto
 # -------------------------------------------------------------------
 
 
 import sys
 
-sys.exit('THIS SCRIPT WAS FOR MIGRATIOIN ... ')
+#sys.exit('THIS SCRIPT WAS FOR MIGRATIOIN ... ')
 
 if __name__ == '__main__':
 
